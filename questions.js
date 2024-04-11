36605d6e350661ebb32d22232625f2ff16cf7d3b
cabbarli123 <166720257+cabbarli123@users.noreply.github.com>
2024-04-11 17:24:01 +0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Mənim ən sevdiyim meyvə? ",
+    answer: "Mandalin ",
+    options: [
+      "Çiyələk",
+      "Armud",
+      "Mandalin",
+      "Alma"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Mənim ən yaxşı etdiyim şey? ",
+    answer: "Futbol oynamaq",
+    options: [
+      "Futbol oynamaq",
+      "Telefon oynamaq",
+      "Dostlarımla zarafatlaşmaq",
+      "Məşq etmək ",
+    ]
+  },
+  {
+    numb: 3,
+    question: "Mənim ən sevdiyim kino(və yaxud dizi)?",
+    answer: "Gaddar",
+    options: [
+      "Kardeşlerim",
+      "Gaddar",
+      "Kuruluş osman",
+      "Mucize doktor"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Mənim ən sevdiyim oyun ",
+    answer: "Brawl stars",
+    options: [
+      "Pubg mobile",
+      "efootball",
+      "Car parking",
+      "Brawl stars"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Mənim ən sevdiyim fənn?",
+    answer: "İngilis dili",
+    options: [
+      "İngilis dili",
+      "İnformatika",
+      "Azərbaycan dili",
+      "Riyaziyyat"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Mənim ən sevdiyim yemək?",
+    answer: "Hamburger",
+    options: [
+      "Pizza",
+      "Kartof fri",
+      "Hamburger",
+      "Yarpaq dolması"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Mənim ən sevdiyim köhnə 2 oyunçu?",
+    answer: "Kaka və Beckham",
+    options: [
+      "Messi və Ronaldinho",
+      "Roberto Carlos  və Cafu",
+      "Maradona və Pele",
+      "Kaka və Beckham"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Ən çox sevdiyim kino cürü?",
+    answer: "Macəra",
+    options: [
+      "Macəra",
+      "Romatik",
+      "Komedi",
+      "Qorxu"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Mənim doğum tarixim?",
+    answer: "2014,01,22",
+    options: [
+      "2014,01,22",
+      "2014,01,12",
+      "2014,01,2",
+      "2014,02,22"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Ən sevdiyim içki?",
+    answer: "coca-cola",
+    options: [
+      "fanta",
+      "coca-cola",
+      "sprite",
+      "Limonat"
+    ]
+  }
+    
+];
